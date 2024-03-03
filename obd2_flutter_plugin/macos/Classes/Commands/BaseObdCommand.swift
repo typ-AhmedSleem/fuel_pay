9c24bf4e90f7712e77475935eff816c5378de9fe
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-03-03 10:11:18 +0200

    refactor: refactored BaseCommand and Protocol commands

--- obd2_flutter_plugin/macos/Classes/Commands/BaseObdCommand.swift
+++ obd2_flutter_plugin/macos/Classes/Commands/BaseObdCommand.swift
@@ -5,4 +5,24 @@
 //  Created by Ahmed Sleem on 08/01/2024.
 //
 
-import Foundation
+protocol BaseObdCommand {
+    
+    /**
+     * Uses bytes available in buffer to perform a suitable sequence of calculations to build the desired result.
+     * [SHOULD BE OVERRIDEN]
+     */
+    func performCalculations() async throws
+    
+    /**
+     *  Returns the formatted result after being calculated.
+     *  [performCalculations SHOULD BE CALLED AT LEAST ONCE BEFORE CALLING THIS METHOD]
+     */
+    func getFormattedResult() -> String
+    
+    /**
+     * Returns the suitable unit for this command.
+     * [SHOULD BE OVERRIDEN]
+     */
+    func getResultUnit() -> String
+    
+}
